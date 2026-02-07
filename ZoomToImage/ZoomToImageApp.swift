7736c18c28a0b57b771b8ff7612ba2d431336e5a
alizaenazet <alizaenazet@icloud.com>
2026-02-07 11:29:55 +0700

    Initial Commit

---
+++ ZoomToImage/ZoomToImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomToImageApp.swift
+//  ZoomToImage
+//
+//  Created by Ali zaenal on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ZoomToImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
